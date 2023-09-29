7af15589401bbb1dc66292b854eac6a79a5363ab
EL MAACHI Ikram <ikram.elmaachi01@gmail.com>
2023-09-29 01:02:11 +0100

    add dashboard page with authLayout

---
+++ src/pages/dashboards/index.tsx
@@ -0,0 +1,18 @@
+import AuthLayout from "@/layouts/auth-layout";
+import React from "react";
+import { BsArrowLeftShort } from "react-icons/bs";
+
+export default function Dashboards() {
+  return (
+    <AuthLayout>
+      <div className="m-5 flex flex-col items-center justify-center gap-5">
+        <h1 className="text-xl">Welcome to Resumint Dashboards</h1>
+
+        <a href="/" className="flex items-center space-x-2 text-blue-500">
+          <BsArrowLeftShort size={24} />
+          <p>Go Back</p>
+        </a>
+      </div>
+    </AuthLayout>
+  );
+}
