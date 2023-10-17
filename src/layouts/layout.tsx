80c1508a1d1e281780d242d37b75a1ba2b0052be
EL MAACHI Ikram <ikram.elmaachi01@gmail.com>
2023-10-17 13:42:49 +0100

    update navbar

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import React from "react";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 interface LayoutI {
   children: React.ReactNode;
@@ -14,14 +17,45 @@ export default function Layout({ children }: LayoutI) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <main className="m-5 flex min-h-screen flex-col p-5">{children}</main>
+      <main className="flex min-h-screen flex-col p-5">{children}</main>
       <Footer />
     </>
   );
 }
 
 function Navbar() {
-  return <div className="flex flex-1 border p-2">Navbar</div>;
+  const { data } = useSession();
+
+  return (
+    <nav className="flex flex-1 justify-between border border-gray-200 bg-white px-3 py-2">
+      <a href="https://flowbite.com/" className="flex items-center">
+        {/* <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="mr-3 h-8"
+            alt="Flowbite Logo"
+          /> */}
+        <span className="self-center whitespace-nowrap text-2xl text-green-600">
+          Resumint
+        </span>
+      </a>
+
+      {data?.user ? (
+        <div className="flex items-center space-x-2">
+          {/* TODO: if there is no user? */}
+          <p className="text-sm">{data.user.name}</p>
+          <Avatar>
+            <AvatarImage
+              sizes="sm"
+              src={data.user.image ?? "/"}
+              alt="@shadcn"
+            />
+          </Avatar>
+        </div>
+      ) : (
+        <Link href="/login">Login</Link>
+      )}
+    </nav>
+  );
 }
 
 function Footer() {
