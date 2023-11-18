d095074055a4345e89fe00938737d30bb997a398
EL MAACHI Ikram <ikram.elmaachi01@gmail.com>
2023-11-18 16:25:19 +0100

    update script seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,211 @@
 import { PrismaClient } from "@prisma/client";
+import { stringify } from "querystring";
 
 const prisma = new PrismaClient();
 
 async function main() {
   await prisma.$connect();
-  // createProfile();
-  // createResumes();
-  // createSections();
-  // createSectionContents();
+  const user = await prisma.user.findFirst();
+  if (!user) {
+    console.log("no user found");
+    return;
+  }
+
+  const userId = user.id;
+
+  // Profile
+  await prisma.profile.deleteMany();
+  await prisma.profile.create({
+    data: {
+      fullName: "Ikram EL MAACHI",
+      email: "ikram.elmaachi01@gmail.com",
+      userId,
+    },
+  });
+
+  // Resume
+  await prisma.resume.deleteMany();
+  const resumes = await prisma.resume.createMany({
+    data: [
+      {
+        title: "Resume01",
+        description: "",
+        userId,
+        templateId: "template-1",
+      },
+      {
+        title: "Resume02",
+        description: "",
+        userId,
+        templateId: "template-2",
+      },
+    ],
+  });
+  if (!resumes) {
+    console.log("Resumes data not found");
+    return;
+  }
+  // fetch resume
+  const resume = await prisma.resume.findFirst({});
+  if (!resume) {
+    console.log("no resume found");
+    return;
+  }
+
+  // Sections
+  await prisma.section.deleteMany();
+  const sections = await prisma.section.createMany({
+    data: [
+      {
+        title: "Education",
+        description: "",
+        type: "EDUCATION",
+        resumeId: resume.id,
+      },
+      {
+        title: "Experience",
+        type: "WORK",
+        resumeId: resume.id,
+      },
+      {
+        title: "Languages",
+        description: "",
+        type: "LANGUAGES",
+        resumeId: resume.id,
+      },
+      {
+        title: "Interests",
+        description: "",
+        type: "INTERESTS",
+        resumeId: resume.id,
+      },
+      {
+        title: "Skills",
+        description: "",
+        type: "SKILLS",
+        resumeId: resume.id,
+      },
+    ],
+  });
+  if (!sections) {
+    console.log("Sections data not found");
+    return;
+  }
+
+  // fetch education section
+  const educationSection = await prisma.section.findFirst({
+    where: {
+      type: "EDUCATION",
+    },
+  });
+  if (!educationSection) {
+    console.log('no section exist with "EDUCATION" type');
+    return;
+  }
+
+  // fetch experience section
+  const experienceSection = await prisma.section.findFirst({
+    where: {
+      type: "WORK",
+    },
+  });
+  if (!experienceSection) {
+    console.log('no section exist with "WORK" type');
+    return;
+  }
+
+  // fetch language section
+  const languageSection = await prisma.section.findFirst({
+    where: {
+      type: "LANGUAGES",
+    },
+  });
+  if (!languageSection) {
+    console.log('no section exist with "LANGUAGE" type');
+    return;
+  }
+
+  // fetch skills section
+  const skillsSection = await prisma.section.findFirst({
+    where: {
+      type: "SKILLS",
+    },
+  });
+  if (!skillsSection) {
+    console.log('no section exist with "SKILLS" type');
+    return;
+  }
+  // Section contents
+  await prisma.sectionContent.deleteMany();
+  const sectionContent = await prisma.sectionContent.createMany({
+    data: [
+      {
+        title: "Master dgree",
+        description: "",
+        establishment: "University of Toronto",
+        location: "Toronto, Canada",
+        from: new Date(),
+        until: new Date(),
+        sectionId: educationSection.id,
+      },
+      {
+        title: "Bachelor's degree ",
+        establishment: "University of Toronto",
+        location: "Toronto, Canada",
+        description: "",
+        from: new Date(),
+        until: new Date(),
+        sectionId: educationSection.id,
+      },
+      {
+        title: "Product manager",
+        description: "",
+        establishment: "Visa",
+        location: "Toronto, Canada",
+        from: new Date(),
+        until: null,
+        sectionId: experienceSection.id,
+      },
+      {
+        title: "Graphic web designer",
+        description: "",
+        establishment: "Blow",
+        location: "Toronto, Canada",
+        from: new Date(),
+        until: new Date(),
+        sectionId: experienceSection.id,
+      },
+      {
+        title: "English",
+        description: "",
+        level: "",
+        sectionId: languageSection.id,
+      },
+      {
+        title: "Portuguese",
+        description: "",
+        level: "",
+        sectionId: languageSection.id,
+      },
+      {
+        title: "Leadership And The Ability To Take Initiative",
+        description: "",
+        level: "",
+        sectionId: skillsSection.id,
+      },
+      {
+        title: "Critical Thinking And Analytical Skills",
+        description: "",
+        level: "",
+        sectionId: skillsSection.id,
+      },
+    ],
+  });
+
+  if (!sectionContent) {
+    console.log("sectionContent data not found");
+    return;
+  }
 }
 
 main()
@@ -17,71 +215,3 @@ main()
   .finally(async () => {
     await prisma.$disconnect();
   });
-
-const createProfile = async () => {
-  await prisma.profile.create({
-    data: {
-      fullName: "Ikram EL MAACHI",
-      email: "ikram.elmaachi01@gmail.com",
-      userId: "clp2ef230000012qfetl4tr9b",
-    },
-  });
-};
-
-const createResumes = async () => {
-  await prisma.resume.createMany({
-    data: [
-      {
-        title: "Resume01",
-        description: "",
-        userId: "clp2ef230000012qfetl4tr9b",
-        templateId: "template-1",
-      },
-      {
-        title: "Resume02",
-        description: "",
-        userId: "clp2ef230000012qfetl4tr9b",
-        templateId: "template-2",
-      },
-    ],
-  });
-};
-
-const createSections = async () => {
-  await prisma.section.createMany({
-    data: [
-      {
-        title: "Education",
-        description: "",
-        type: "EDUCATION",
-        resumeId: "clp44ob520000ldy5lwr9pqez",
-      },
-      {
-        title: "Experience",
-        type: "WORK",
-        resumeId: "clp44ob520000ldy5lwr9pqez",
-      },
-    ],
-  });
-};
-
-const createSectionContents = async () => {
-  await prisma.sectionContent.createMany({
-    data: [
-      {
-        title: "Master dgree",
-        description: "",
-        from: new Date(),
-        until: new Date(),
-        sectionId: "clp44szwd0000szsil27kp4zw",
-      },
-      {
-        title: "Graphic web designer",
-        description: "",
-        from: new Date(),
-        until: null,
-        sectionId: "clp44szwd0001szsixr2bpg93",
-      },
-    ],
-  });
-};
